b948526feefdff074ddb0b67e9467924d6ccb432
Nikolay Shapenkov <k89536970432@yandex.ru>
2023-04-11 09:53:40 +0300

    На первую итерацию, ПР6

--- scripts/index.js
+++ scripts/index.js
@@ -44,20 +44,19 @@ function handleOverlyPopupClick(event) {
   if(event.target === event.currentTarget) {
   closePopup(popup);
 }
-}
+};
 
 function handleOverlyPopupCardsClick(event) {
   if(event.target === event.currentTarget) {
   closePopup(popupAddCard);
 }
-}
-
+};
 
 function handleOverlyPopupLookCardsClick(event) {
   if(event.target === event.currentTarget) {
   closePopup(popupLookCard);
 }
-}
+};
 
 function handlePopupKeydown(event) {
   if(event.key === 'Escape') {
@@ -65,7 +64,6 @@ function handlePopupKeydown(event) {
     closePopup(popupAddCard);
     closePopup(popupLookCard);
   }
-  console.log('РАБОТАЕТ');
 };
 
 aboutButton.addEventListener('click', handleAboutButtonClick);
@@ -81,7 +79,8 @@ function handleAboutButtonSubmitFormProfile (evt) {
   profileText.textContent = fieldDescriptionInput.value;//заполняем Описание на странице данными из поля описания
 
   closePopupProfile();
-}
+};
+
 formElementt.addEventListener('submit', handleAboutButtonSubmitFormProfile);
 
 // Пятая ПР
@@ -92,7 +91,6 @@ const formPopupAddCardsElement = popupAddCard.querySelector('.popup-cards__form'
 const popupCardFieldNameInput = popupAddCard.querySelector('.popup-cards__field_type_name');//Поле ввода в попапе для добавления карточек с названием
 const popupCardFieldLinkInput = popupAddCard.querySelector('.popup-cards__field_type_link');//Поле ввода в попапе для добавления карточек со ссылкой
 const buttonCloseCard = popupAddCard.querySelector('.popup-cards__close');//кнопка для закрытия попапа для добавления карточек
-
 
 const clickLookImage = document.querySelector('.element__image');//Зона клика (картинка) для просмотра фото
 const popupImagePictures = popupLookCard.querySelector('.popup-image__picture');//Картинка в попапе для просмотра фото
@@ -182,92 +180,5 @@ buttonCloseImage.addEventListener('click', closePopupLookImage);//закрыти
 
 
 
-//6ПРОЕКТАНАЯ РАБОТА
 
-/*const formNumberOne = document.querySelector('.popup-cards__form');
-const inputNumberOne = formNumberOne.querySelector('.popup-cards__field_type_name');
-const inputNumberToo = formNumberOne.querySelector('.popup-cards__field_type_link');*/
-
-//массив с данными
-
-const formValidationConfig = {
-formSelector: '.popup__form',
-inputSelector: '.popup__field',
-submitButtonSelector: '.popup__save',
-inactiveButtonClass: 'popup__save_invalid',
-inputErrorClass: 'popup__field_type_error',//класс который стилизует неправильное поле ИЗНАЧАЛЬНО ОТКЛЮЧЕН (полоска становится красной)
-errorActiveClass: 'popup__input-error_visible',//класс который делает видимым Спан с ошибкой
-};
-
-const enableValidation = ({formSelector, ...rest}) => {
-  const formList = Array.from(document.querySelectorAll(formSelector));
-  formList.forEach(function(formElement) {
-   setEventListeners(formElement, rest);
-  });
-};
-
-//Функция, принимающая форму и вешающая обработчики и делающая сразу неактивной кнопку, если есть невалидный инпут
-const setEventListeners = (formElement, {inputSelector, submitButtonSelector, ...rest}) => {
-  const inputList = Array.from(formElement.querySelectorAll(inputSelector));
-  const buttonElement = formElement.querySelector(submitButtonSelector);
-  toggleButtonState(inputList, buttonElement, rest);
-  inputList.forEach(function(inputElement) {
-    inputElement.addEventListener('input', function() {
-      isValid(formElement, inputElement, rest);
-      toggleButtonState(inputList, buttonElement, rest);
-    })
-  });
-};
-
-//Функция принимает в себя любую форму, находит инпуты и вешает на них обработчики
-
-//Функция, добавляющая класс к элементу, чтобы стилизовать невалидное поле и делающая контейнер с ошибкой видимым
-const showInputError = (formElement, inputElement, errorMessage, {inputErrorClass, errorActiveClass}) => {
-  const inputErrorContainer = formElement.querySelector(`.${inputElement.id}-error`);// Находим класс спана для конкретного поля
-  inputElement.classList.add(inputErrorClass);
-  inputErrorContainer.classList.add(errorActiveClass);
-  inputErrorContainer.textContent = errorMessage;
-};
-
-//Функция, удаляющая класс у элемента
-const hideInputError = (formElement, inputElement, {inputErrorClass, errorActiveClass}) => {
-  const inputErrorContainer = formElement.querySelector(`.${inputElement.id}-error`);// Находим класс спана для конкретного поля
-  inputElement.classList.remove(inputErrorClass);
-  inputErrorContainer.classList.remove(errorActiveClass);
-  inputErrorContainer.textContent = '';
-};
-
-//функция, проверяющая валидность и удаляющая или добавляющая класс для стилизации невалидного поля
-
-const isValid = (formElement, inputElement, rest) => {
-  if(!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage, rest);
-  } else {
-    hideInputError(formElement, inputElement, rest);
-  };
-};
-
-//Функция, пербирающая инпуты формы и выдающая true, если находит невалидное поле.
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-  return !inputElement.validity.valid;
-  });
-};
-
-//Функция, которая делает кнопку неактивной
-const toggleButtonState = (inputList, buttonElement, {inactiveButtonClass, ...rest}) => {
-  // Если есть хотя бы один невалидный инпут
-  if (hasInvalidInput(inputList)) {
-    // сделай кнопку неактивной
-    buttonElement.classList.add(inactiveButtonClass);
-    buttonElement.setAttribute('disabled', true);
-  } else {
-    // иначе сделай кнопку активной
-    buttonElement.classList.remove(inactiveButtonClass);
-    buttonElement.removeAttribute('disabled', true);
-  }
-  console.log('Вообще работает?');
-}; 
-
-enableValidation(formValidationConfig);
 
